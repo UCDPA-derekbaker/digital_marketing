23bb5e8cc16b4939bb2bcb1696d02ea7a944c758
UCDPA-derekbaker <derek.baker@concern.net>
2021-03-05 19:34:30 +0000

    Test 3

--- data_analytics_for_marketing.py
+++ data_analytics_for_marketing.py
@@ -14,4 +14,34 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
-#test
+
+# Print the head of the homelessness data
+print(homelessness.head())
+
+# Print information about homelessness
+print(homelessness.info())
+
+# Print the shape of homelessness
+print(homelessness.shape)
+
+# Print a description of homelessness
+print(homelessness.describe())
+
+###############################################
+# Import pandas using the alias pd
+#install packages pandas as pd
+
+# Print the values of homelessness
+print(homelessness.values)
+
+# Print the column index of homelessness
+print(homelessness.columns)
+
+# Print the row index of homelessness
+print(homelessness.index)
+
+# Sort homelessness by individual
+homelessness_ind = homelessness.sort_values("individuals")
+
+# Print the top few rows
+print(homelessness_ind.head())
